9e59e2cd75a5d7466c7de52212f663ba945600df
Your Name <you@example.com>
2023-05-23 17:21:17 +0500

    scripts

---
+++ data/download_data_smpl_x.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+urle () { [[ "${1}" ]] || return 1; local LANG=C i x; for (( i = 0; i < ${#1}; i++ )); do x="${1:i:1}"; [[ "${x}" == [a-zA-Z0-9.~-] ]] && echo -n "${x}" || printf '%%%02X' "'${x}"; done; echo; }
+
+read -p "Username:" username
+read -p "Password:" -s password
+
+username=$(urle $username)
+password=$(urle $password)
+
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=smplx&sfile=models_smplx_v1_1.zip' -O 'models_smplx_v1_1.zip' --no-check-certificate --continue
+unzip shapy_data.zip
